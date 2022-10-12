546f65ea3e21d16dfcf7e04d04dd173ae7ba0c95
Luca Xiang <luca.xiang@outlook.com>
2022-10-13 00:16:09 +0200

    feat: windows example embryonic

--- examples/windows/main.rs
+++ examples/windows/main.rs
@@ -2,8 +2,33 @@
 fn main() {
     println!("please run this example on windows");
 }
-
 #[cfg(target_os = "windows")]
+use windows::Win32::Foundation::HWND;
+#[cfg(target_os = "windows")]
+use windows::Win32::UI::Input::KeyboardAndMouse::{
+    RegisterHotKey, UnregisterHotKey, MOD_ALT, MOD_CONTROL,
+};
+
 fn main() {
-    println!("Hello World!");
+    let modifiers = MOD_CONTROL | MOD_ALT;
+    unsafe {
+        const ID: i32 = 0;
+
+        {
+            let result = RegisterHotKey(HWND::default(), ID, modifiers, 'A'.into());
+            if result.0 == 0 {
+                panic!("RegisterHotKey failed");
+            } else {
+                println!("RegisterHotKey succeeded");
+            }
+        }
+        {
+            let result = UnregisterHotKey(HWND::default(), ID);
+            if result.0 == 0 {
+                panic!("UnregisterHotKey failed");
+            } else {
+                println!("UnregisterHotKey succeeded");
+            }
+        }
+    }
 }
