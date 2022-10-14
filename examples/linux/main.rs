ad8404f1b5faf1ad847a607fe38bd0b282be51d8
Luca Xiang <luca.xiang@outlook.com>
2022-10-14 02:55:03 +0200

    test linux

--- examples/linux/main.rs
+++ examples/linux/main.rs
@@ -1,14 +1,29 @@
-#[cfg(not(target_os = "linux"))]
-mod no_support;
-#[cfg(not(target_os = "linux"))]
-pub use no_support::Utils;
-
 #[cfg(target_os = "linux")]
-mod support;
+use std::ptr;
 #[cfg(target_os = "linux")]
-pub use support::Utils;
-
+use x11::xlib;
+#[cfg(target_os = "linux")]
 fn main() {
-    Utils::register_hotkey();
-    Utils::unregister_hotkey();
+    unsafe {
+        let display = xlib::XOpenDisplay(ptr::null());
+        let root = xlib::XDefaultRootWindow(display);
+        let keycode = xlib::XKeysymToKeycode(display, 65) as i32;
+        let result = xlib::XGrabKey(
+            display,
+            keycode,
+            0,
+            root,
+            0,
+            xlib::GrabModeAsync,
+            xlib::GrabModeAsync,
+        );
+        if result == 0 {
+            println!("RegisterHokey succeeded");
+        } else {
+            println!("RegisterHokey failed");
+        }
+    }
 }
+
+#[cfg(not(target_os = "macos"))]
+fn main() {}
